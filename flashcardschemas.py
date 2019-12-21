26afe57a4021757c70150a5069a01b59ae0ce88e
Eli <elimaynard923@gmail.com>
2019-12-21 14:53:16 -0800

    Incorporate requests

--- flashcardschemas.py
+++ flashcardschemas.py
@@ -1,6 +1,6 @@
 from collections import namedtuple
 
-ChineseRecognitionOnly = namedtuple('ChineseRecognitionOnly', 'simplified traditional pinyin zhuyin simplified_stroke traditional_stroke definition extra_definition words_with_same_pronunciation words_with_same_definition tags')
+ChineseRecognitionOnly = namedtuple('ChineseRecognitionOnly', 'simplified traditional pinyin zhuyin definition extra_definition words_with_same_pronunciation words_with_same_definition tags')
 
 def word_to_ChineseRecognitionOnly(word, tags):
   'converts `ChineseWord` word to ChineseRecognitionOnly tuple'
@@ -8,3 +8,5 @@ def word_to_ChineseRecognitionOnly(word, tags):
   tags = ' '.join(tags)
   traditional = '' if word.simplified == word.traditional else word.traditional
   return ChineseRecognitionOnly(word.simplified, traditional, word.pinyin, word.zhuyin, definition, '', '', '', tags)
+
+ChineseRecognitionOnlyWithStroke = namedtuple('ChineseRecognitionOnly', 'simplified traditional pinyin zhuyin simplified_stroke traditional_stroke definition extra_definition words_with_same_pronunciation words_with_same_definition tags')
--- stroke.py
+++ stroke.py
@@ -1,27 +1,25 @@
 import requests
-import re
 import os
-import time
 
 def get_stroke(hanzi):
-  return '&nbsp;'.join(map(get_stroke_single, hanzi))
+  # Add a space between images because it looks nicer
+  return ' '.join(map(get_stroke_single, hanzi))
 
 def get_stroke_single(hanzi):
   if not os.path.exists('content/'):
     os.mkdir('content/')
 
-  hanzi_id = str(ord(hanzi))
-  gif_loc = 'content/' + hanzi_id + '.gif'
-
+  gif_loc = f'content/{hanzi}.gif'
   if not os.path.exists(gif_loc):
-    response = requests.get('https://www.mdbg.net/chinese/rsc/img/stroke_anim/{}.gif'.format(hanzi_id))
+    hanzi_id = str(ord(hanzi))
+    response = requests.get(f'https://www.mdbg.net/chinese/rsc/img/stroke_anim/{hanzi_id}.gif')
 
-    if not response.ok and response.content:
-      print(u"Unable to fetch stroke for {}, trying again...".format(hanzi))
+    if not (response.ok and response.content):
+      print(u"Unable to fetch stroke for {hanzi}.")
       return ""
 
     open(gif_loc, 'wb').write(response.content)
 
   # The gif should be moved into Anki's collections.media folder,
   # where it may be referenced within the note simply by filename, not location
-  return '<img src="' + hanzi_id + '.gif">'
+  return f'<img src="{hanzi}.gif">'
