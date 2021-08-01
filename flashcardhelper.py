833058eb63e44ad9574cfa427b4b4de9a6b53cfc
samm81 <samwmaynard@gmail.com>
2021-08-01 00:01:40 -0700

    2021-08-01

--- flashcardhelper.py
+++ flashcardhelper.py
@@ -13,159 +13,182 @@ t2s = OpenCC('t2s')
 
 State = Enum('State', 'BEGIN PICK_WORD PICK_ENGLISH PICKED')
 
+
 class FlashcardHelper(cmd.Cmd):
-  filename = 'cards.csv'
-  intro = 'Welcome to flashcard helper. Type help or ? to list commands\n'
-  prompt = '入 '
+    filename = 'cards.csv'
+    intro = 'Welcome to flashcard helper. Type help or ? to list commands\nTODO: de-dupe sim/trad\n'
+    prompt = '入 '
 
-  words = []
-  word = None
-  choice = None
-  state = State.BEGIN
-  filemode = False
+    words = []
+    word = None
+    choice = None
+    state = State.BEGIN
+    filemode = False
+    tags = ''
 
-  def transition_to(self, to_state):
-    if to_state is State.BEGIN:
-      self.words = []
-      self.state = to_state
-    elif to_state is State.PICK_WORD:
-      self.state = to_state
-    elif to_state is State.PICK_ENGLISH:
-      self.state = to_state
-    elif to_state is State.PICKED:
-      if self.state is State.PICK_WORD:
-        self.word = self.words[self.choice]
-        return self.handle_word()
-      elif self.state is State.PICK_ENGLISH:
-        return self.handle_english()
+    CSV_DELIMITER = '\t'
 
-  def transitions_to(to_state):
-    'convenience decorator for functions that only transition to one state'
-    def transition_decorator(func):
-      @wraps(func)
-      def wrapper(self, *args, **kwargs):
-        retval = func(self, *args, **kwargs)
-        self.transition_to(to_state)
-        return retval
-      return wrapper
-    return transition_decorator
+    def transition_to(self, to_state):
+        if to_state is State.BEGIN:
+            self.words = []
+            self.state = to_state
+        elif to_state is State.PICK_WORD:
+            self.state = to_state
+        elif to_state is State.PICK_ENGLISH:
+            self.state = to_state
+        elif to_state is State.PICKED:
+            if self.state is State.PICK_WORD:
+                self.word = self.words[self.choice]
+                return self.handle_word()
+            elif self.state is State.PICK_ENGLISH:
+                return self.handle_english()
 
-  def handle_word(self, word_to_flashcard=word_to_ChineseRecognitionOnly):
-    if len(self.word.english) is 1:
-      flashcard = word_to_flashcard(self.word, [])
-      retval = self.save_to_file(flashcard)
-      self.transition_to(State.BEGIN)
-      return retval
-    else:
-      print('which definitions should go in the main definition?')
-      self.display_options(self.word.english, multiple=True)
-      self.transition_to(State.PICK_ENGLISH)
+    def transitions_to(to_state):
+        'convenience decorator for functions that only transition to one state'
+        def transition_decorator(func):
+            @wraps(func)
+            def wrapper(self, *args, **kwargs):
+                retval = func(self, *args, **kwargs)
+                self.transition_to(to_state)
+                return retval
+            return wrapper
+        return transition_decorator
 
-  @transitions_to(State.BEGIN)
-  def handle_english(self):
-    primaries = [ english for i, english in enumerate(self.word.english) if i in self.choice ]
-    secondaries = [ english for i, english in enumerate(self.word.english) if i not in self.choice ]
-    definition = '; '.join(primaries)
-    extra_definition = '; '.join(secondaries)
-    flashcard = ChineseRecognitionOnly(self.word.simplified, self.word.traditional, self.word.pinyin, self.word.zhuyin, definition, extra_definition, '', '', '')
-    return self.save_to_file(flashcard)
+    def handle_word(self, word_to_flashcard=word_to_ChineseRecognitionOnly):
+        if len(self.word.english) is 1:
+            flashcard = word_to_flashcard(self.word, self.tags)
+            retval = self.save_to_file(flashcard)
+            self.transition_to(State.BEGIN)
+            return retval
+        else:
+            print('which definitions should go in the main definition?')
+            self.display_options(self.word.english, multiple=True)
+            self.transition_to(State.PICK_ENGLISH)
 
-  def display_options(self, optionstrings, multiple=False):
-    print('\n'.join('{}) {}'.format(i, optionstring) for i, optionstring in enumerate(optionstrings)))
-    if multiple:
-      print('type `pick [num] ([num] [num]) to pick option(s), or type any other command')
-    else:
-      print('type `pick [num]` to pick an option, or type any other command')
+    @transitions_to(State.BEGIN)
+    def handle_english(self):
+        primaries = [english for i, english in enumerate(
+            self.word.english) if i in self.choice]
+        secondaries = [english for i, english in enumerate(
+            self.word.english) if i not in self.choice]
+        definition = '; '.join(primaries)
+        extra_definition = '; '.join(secondaries)
+        tags = self.tags
+        flashcard = ChineseRecognitionOnly(self.word.simplified, self.word.traditional,
+                                           self.word.pinyin, self.word.zhuyin, definition, extra_definition, '', '', tags)
+        return self.save_to_file(flashcard)
 
-  def save_to_file(self, flashcard, filename=filename):
-    confirm = input(f'saving {flashcard} to file {filename}, looks ok ([Y]n) ? ')
-    if confirm.strip().lower() in ['n', 'no']:
-      print('not saved.')
-    else:
-      with open(filename, 'a', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(flashcard)
-      print('saved!')
-    # if we're operating over a file our job for this word is done and we wanna return control to our file loop!
-    if self.filemode:
-      return True
+    def display_options(self, optionstrings, multiple=False):
+        print('\n'.join('{}) {}'.format(i, optionstring)
+                        for i, optionstring in enumerate(optionstrings)))
+        if multiple:
+            print(
+                'type `pick [num] ([num] [num]) to pick option(s), or type any other command')
+        else:
+            print(
+                'type `pick [num]` to pick an option, or type any other command')
 
-  def do_find(self, arg):
-    'find a chinese character or phrase: find ni3hao'
-    if not(transcriptions.is_pinyin(arg) or hanzi.is_simplified(arg) or hanzi.is_traditional(arg)):
-      print('input is not well formed pinyin or well formed Chinese characters')
-      return # don't transition state
-    words = find_words(arg)
-    if len(words) is 0:
-      print('could not find character or phrase')
-      return # don't transition state
-    self.words = words
-    if len(words) is 1 and self.filemode:
-      self.choice = 0
-      self.state = State.PICK_WORD
-      return self.transition_to(State.PICKED)
-    else:
-      self.display_options('{}\t{}\t{}\t{}\t{}'.format(*word) for word in words)
-      self.transition_to(State.PICK_WORD)
+    def save_to_file(self, flashcard, filename=filename):
+        confirm = input(
+            f'saving {flashcard} to file {filename}, looks ok ([Y]n) ? ')
+        if confirm.strip().lower() in ['n', 'no']:
+            print('not saved.')
+        else:
+            with open(filename, 'a', newline='') as csvfile:
+                writer = csv.writer(csvfile, delimiter=self.CSV_DELIMITER)
+                writer.writerow(flashcard)
+            print('saved!')
+        # if we're operating over a file our job for this word is done and we wanna return control to our file loop!
+        if self.filemode:
+            return True
 
-  # TODO pick all
-  def do_pick(self, arg):
-    if not (self.state is State.PICK_WORD or self.state is State.PICK_ENGLISH):
-      print('no options to pick from right now!')
-      return # don't transition state
-    choices = list(map(int, arg.split()))
-    if len(choices) is 0:
-      print('must chose an option!')
-      return # don't transition state
-    elif self.state is State.PICK_WORD and len(choices) is 1:
-      self.choice = choices[0]
-    elif self.state is State.PICK_WORD and len(choices) > 1:
-      print('can only chose one option!')
-      return # don't transition state
-    else:
-      self.choice = choices
-    return self.transition_to(State.PICKED)
+    def do_find(self, arg):
+        'find a chinese character or phrase: find ni3hao'
+        if not(transcriptions.is_pinyin(arg) or hanzi.is_simplified(arg) or hanzi.is_traditional(arg)):
+            print('input is not well formed pinyin or well formed Chinese characters')
+            return  # don't transition state
+        words = find_words(arg)
+        if len(words) is 0:
+            print('could not find character or phrase')
+            return  # don't transition state
+        self.words = words
+        if len(words) is 1 and self.filemode:
+            self.choice = 0
+            self.state = State.PICK_WORD
+            return self.transition_to(State.PICKED)
+        else:
+            self.display_options('{}\t{}\t{}\t{}\t{}'.format(*word)
+                                 for word in words)
+            self.transition_to(State.PICK_WORD)
 
-  def do_make(self, arg):
-    'make a custom flashcard: make 什么什么'
-    if not hanzi.has_chinese(arg):
-      print('input is not well formed chinese characters')
-      return # don't transition state
-    simplified = t2s.convert(arg)
-    traditional = s2t.convert(arg)
-    pinyin = hanzi.to_pinyin(arg)
-    zhuyin = hanzi.to_zhuyin(arg)
-    english = input('english definition: ')
-    self.save_to_file(ChineseRecognitionOnly(simplified, traditional, pinyin, zhuyin, english, '', '', '', ''))
-    self.transition_to(State.BEGIN)
+    # TODO pick all
+    def do_pick(self, arg):
+        if not (self.state is State.PICK_WORD or self.state is State.PICK_ENGLISH):
+            print('no options to pick from right now!')
+            return  # don't transition state
+        choices = list(map(int, arg.split()))
+        if len(choices) is 0:
+            print('must chose an option!')
+            return  # don't transition state
+        elif self.state is State.PICK_WORD and len(choices) is 1:
+            self.choice = choices[0]
+        elif self.state is State.PICK_WORD and len(choices) > 1:
+            print('can only chose one option!')
+            return  # don't transition state
+        else:
+            self.choice = choices
+        return self.transition_to(State.PICKED)
 
-  def do_EOF(self, arg):
-    'exit'
-    print('exiting.')
-    return True
+    def do_make(self, arg):
+        'make a custom flashcard: make 什么什么'
+        if not hanzi.has_chinese(arg):
+            print('input is not well formed chinese characters')
+            return  # don't transition state
+        simplified = t2s.convert(arg)
+        traditional = s2t.convert(arg)
+        pinyin = hanzi.to_pinyin(arg)
+        zhuyin = hanzi.to_zhuyin(arg)
+        english = input('english definition: ')
+        self.save_to_file(ChineseRecognitionOnly(
+            simplified, traditional, pinyin, zhuyin, english, '', '', '', self.tags))
+        self.transition_to(State.BEGIN)
 
-  def do_exit(self, arg):
-    'exit'
-    print('exiting.')
-    return True
+    def do_tags(self, arg):
+        'set tags: tags IntegratedChinese::1.4 IntermediateReader::8'
+        self.tags = arg
 
-  def register_filemode(self):
-    'used when reading a set of words from a file so that the cmd knows to finish after saving a card'
-    self.filemode = True
+    def do_EOF(self, arg):
+        'exit'
+        self.do_exit(arg)
+
+    def do_exit(self, arg):
+        'exit'
+        print('exiting.')
+        return True
+
+    def register_filemode(self):
+        """
+        used when reading a set of words from a file so that the cmd knows to
+        finish after saving a card
+        """
+        self.filemode = True
+
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser(description='Command line helper for making Chinese Anki flashcards')
-  parser.add_argument('file', nargs='?', type=argparse.FileType('r', encoding='utf-8'), help='newline separated file of words to add')
-  args = parser.parse_args()
+    parser = argparse.ArgumentParser(
+        description='Command line helper for making Chinese Anki flashcards')
+    parser.add_argument('file', nargs='?', type=argparse.FileType(
+        'r', encoding='utf-8'), help='newline separated file of words to add')
+    args = parser.parse_args()
 
-  helper_cmd = FlashcardHelper()
-  if not args.file:
-    helper_cmd.cmdloop()
-  else:
-    print(f'loading {args.file.name}...')
-    words = args.file.read().splitlines()
-    helper_cmd.register_filemode()
-    for i, word in enumerate(words):
-      print(f'looking up {word} (word {i} of {len(words)})')
-      helper_cmd.onecmd(f'find {word}')
-      helper_cmd.cmdloop(intro='')
+    helper_cmd = FlashcardHelper()
+    if not args.file:
+        helper_cmd.cmdloop()
+    else:
+        print(f'loading {args.file.name}...')
+        words = args.file.read().splitlines()
+        helper_cmd.register_filemode()
+        for i, word in enumerate(words):
+            print(f'looking up {word} (word {i} of {len(words)})')
+            helper_cmd.onecmd(f'find {word}')
+            helper_cmd.cmdloop(intro='')
--- flashcardschemas.py
+++ flashcardschemas.py
@@ -1,9 +1,8 @@
 from collections import namedtuple
 
 ChineseRecognitionOnly = namedtuple('ChineseRecognitionOnly', 'simplified traditional pinyin zhuyin definition extra_definition words_with_same_pronunciation words_with_same_definition tags')
-def word_to_ChineseRecognitionOnly(word, tags):
+def word_to_ChineseRecognitionOnly(word, tags_string):
   'converts `ChineseWord` word to ChineseRecognitionOnly tuple'
   definition = '; '.join(word.english)
-  tags = ' '.join(tags)
   traditional = '' if word.simplified == word.traditional else word.traditional
-  return ChineseRecognitionOnly(word.simplified, traditional, word.pinyin, word.zhuyin, definition, '', '', '', tags)
+  return ChineseRecognitionOnly(word.simplified, traditional, word.pinyin, word.zhuyin, definition, '', '', '', tags_string)
